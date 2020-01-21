c6e5a876d8fbaa292c56a0c042dc11a6ae7ab477
David <davidnyangi@gmail.com>
2020-01-21 17:23:05 +0300

    Adding react-icons

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,3 +1,7 @@
+import { MdTerrain } from "react-icons/md";
+import { TiWeatherSnow } from "react-icons/ti";
+import { FaCalendarAlt } from "react-icons/fa";
+
 const percentToDecimal = (decimal) => {
     return ((decimal * 100) + '%')
 }
@@ -8,15 +12,16 @@ const calcGoalProgress = (total,goal) => {
 export const SkiDayCount = ({total,powder,backcountry,goal}) => (
     <div className="ski-day-count">
         <div className="total-days">
-            <span>{total}</span>
+            <span>{total}</span><FaCalendarAlt/>
             <span>days</span>
         </div>
         <div className="powder-days">
-            <span>{powder}</span>
+            <span>{powder}</span><TiWeatherSnow/>
             <span>days</span>
         </div>
         <div className="backcountry-days">
             <span>{backcountry}</span>
+                <MdTerrain/>
             <span>hiking day</span>
         </div>
         <div>
